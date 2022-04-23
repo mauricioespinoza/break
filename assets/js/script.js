17baa9ac00a0b1b9f7334e5ca23c99e351ee6501
Mauricio Espinoza <34545968+mauricioespinoza@users.noreply.github.com>
2022-04-23 12:54:52 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,7 @@ function horaActual() {
    //Gatillo la alarma
     if (hora == this.horaD && minuto == this.minutoD && segundo == 00) {
         //inicializo una variable de audio
-		var audio = new Audio("/assets/others/alarm.mp3");
+		var audio = new Audio("https://github.com/mauricioespinoza/break/blob/main/assets/others/alarm.mp3");
 		audio.play();
         audio.addEventListener("ended", function(){
             desplegar();
